ea7aa47cf0ac7d7abc0a99bee8e2d0980285b5d4
indahalmrifah <31643168+indahalmrifah@users.noreply.github.com>
2019-10-07 09:54:25 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -19,12 +19,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="practice-environmental-qwareness-to-our.html"}
-if(x=="2"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="3"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="4"){xxx="the-way-to-start-business-at-home.html"}a_to_ve[i].href="https://internetgratistekomsel.blogspot.com/2018/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="how-to-choose-mesothelioma-law-firm.html"}
+if(x=="2"){xxx="how-to-choose-mesothelioma-law-firm.html"}
+if(x=="3"){xxx="mesothelioma-law-firms-obtaining-your.html"}
+if(x=="4"){xxx="reasons-you-may-need-mesothelioma-law.html"}
+if(x=="5"){xxx="mesothelioma-law-firms-what-everyone.html"}a_to_ve[i].href="https://amerika.tandooriez.me/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
